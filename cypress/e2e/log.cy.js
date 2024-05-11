a999dd53faf2c6682f361dc6fccdfb30d7c5a270
jurachik31 <168196847+jurachik31@users.noreply.github.com>
2024-05-11 18:40:02 +0300

    Add files via upload

---
+++ cypress/e2e/log.cy.js
@@ -0,0 +1,64 @@
+describe('Проверка авторизации', function () {
+
+    it('Верный пароль и верный логин', function () {
+         cy.visit('https://login.qa.studio');
+         cy.get('#forgotEmailButton').should('have.css', 'color', 'rgb(0, 85, 152)');
+         cy.get('#mail').type('german@dolnikov.ru');
+         cy.get('#pass').type('iLoveqastudio1');
+         cy.get('#loginButton').click();
+         cy.get('#messageHeader').should('be.visible');
+         cy.get('#messageHeader').contains('Авторизация прошла успешно');
+         cy.get('#exitMessageButton > .exitIcon').should('be.visible');
+     })
+ 
+     it('Верный логин и неверный пароль', function () {
+         cy.visit('https://login.qa.studio');
+         cy.get('#forgotEmailButton').should('have.css', 'color', 'rgb(0, 85, 152)');
+         cy.get('#mail').type('german@dolnikov.ru');
+         cy.get('#pass').type('iLoveqastudio2');
+         cy.get('#loginButton').click();
+         cy.get('#messageHeader').should('be.visible');
+         cy.get('#messageHeader').contains('Такого логина или пароля нет');
+         cy.get('#exitMessageButton > .exitIcon').should('be.visible');
+     })
+     it('Не верный логин и верный пароль', function () {
+        cy.visit('https://login.qa.studio');
+        cy.get('#forgotEmailButton').should('have.css', 'color', 'rgb(0, 85, 152)');
+        cy.get('#mail').type('german@dlnikov.ru');
+        cy.get('#pass').type('iLoveqastudio1');
+        cy.get('#loginButton').click();
+        cy.get('#messageHeader').should('be.visible');
+        cy.get('#messageHeader').contains('Такого логина или пароля нет');
+        cy.get('#exitMessageButton > .exitIcon').should('be.visible');
+    })
+ 
+     it('Валидация на наличие @', function () {
+         cy.visit('https://login.qa.studio');
+         cy.get('#forgotEmailButton').should('have.css', 'color', 'rgb(0, 85, 152)');
+         cy.get('#mail').type('germandolnikov.ru');
+         cy.get('#pass').type('iLoveqastudio');
+         cy.get('#loginButton').click();
+         cy.get('#messageHeader').should('be.visible');
+         cy.get('#messageHeader').contains('Нужно исправить проблему валидации');
+     })
+     it('Восстановление пароля', function () {
+         cy.visit('https://login.qa.studio');
+         cy.get('#forgotEmailButton').should('have.css', 'color', 'rgb(0, 85, 152)');
+         cy.get('#forgotEmailButton').click();
+         cy.get('#mailForgot').type('german@dolnikov.ru');
+         cy.get('#restoreEmailButton').click();
+         cy.get('#messageHeader').contains('Успешно отправили пароль на e-mail');
+         cy.get('#exitMessageButton > .exitIcon').should('be.visible');
+     }) 
+     it('Вести в логин заглавные буквы ', function () {
+        cy.visit('https://login.qa.studio');
+        cy.get('#forgotEmailButton').should('have.css', 'color', 'rgb(0, 85, 152)');
+        cy.get('#mail').type('GerMan@Dolnikov.ru');
+        cy.get('#pass').type('iLoveqastudio1');
+        cy.get('#loginButton').click();
+        cy.get('#messageHeader').should('be.visible');
+        cy.get('#messageHeader').contains('Авторизация прошла успешно');
+        cy.get('#exitMessageButton > .exitIcon').should('be.visible');
+     }) 
+     
+ })
